ebbaf3d5acfc73a86226b8c1172d0cdb6d06b097
Jasha B <53990586+Cowboycommit@users.noreply.github.com>
2020-10-12 16:52:42 -0700

    bash auto run files (set to the 3 FPS settings)
    Here, for ease of use to check the app at the three standard FPS settings I have created bash files for easy execution. It you want to mix and match FPS's  of each model you can find more instructions in the README.md

---
+++ party_starter32.sh
@@ -0,0 +1,3 @@
+python3 src/main.py -f src/models/intel/face-detection-adas-0001/FP32/face-detection-adas-0001.xml -fl src/models/intel/landmarks-regression-retail-0009/FP32/landmarks-regression-retail-0009.xml -hp src/models/intel/head-pose-estimation-adas-0001/FP32/head-pose-estimation-adas-0001.xml -g src/models/intel/gaze-estimation-adas-0002/FP32/gaze-estimation-adas-0002.xml -i src/bin/demo.mp4 -d GPU 
+
+
