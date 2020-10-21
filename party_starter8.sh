bf6521a7d3fb8a928ed32e9accb46790a676300c
Jasha B <53990586+Cowboycommit@users.noreply.github.com>
2020-10-21 22:04:00 +1100

    Updated README.md
    Project completion/submission updates + requirements updated and starter files also updated

--- party_starter8.sh
+++ party_starter8.sh
@@ -1,9 +1,3 @@
-
-
-source ~/intel/openvino_2020.4.287/bin/setupvars.sh
-
-cd ~/Desktop/JashaBCPC
-
 python3 src/main.py -f src/models/intel/face-detection-adas-0001/FP16-INT8/face-detection-adas-0001.xml -fl src/models/intel/landmarks-regression-retail-0009/FP16-INT8/landmarks-regression-retail-0009.xml -hp src/models/intel/head-pose-estimation-adas-0001/FP16-INT8/head-pose-estimation-adas-0001.xml -g src/models/intel/gaze-estimation-adas-0002/FP16-INT8/gaze-estimation-adas-0002.xml -i src/bin/demo.mp4
 
 
